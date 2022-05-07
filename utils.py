f44626f0a467132a58418e38d76eca330a58445e
ironrivals <ironrivals.bot@gmail.com>
2022-05-07 09:58:53 -0400

    bugfix

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 import asyncio
-from types import NoneType
 from discord import Member
 
 async def getAlias(user: Member):
     alias = None
-    if type(user.nick) != NoneType:
+    if type(user.nick) != type(None):
         alias = user.nick
     else:
         alias = user.name
